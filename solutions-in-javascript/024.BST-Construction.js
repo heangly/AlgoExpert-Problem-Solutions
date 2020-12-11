39a086d807debffc80bbe6ae9fa205c79e2b6da0
Heang Ly <heanglypa@gmail.com>
2020-12-11 08:29:01 -0500

    BST-construction

---
+++ solutions-in-javascript/024.BST-Construction.js
@@ -0,0 +1,90 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+    if (value === this.value) return true;
+
+    if (value < this.value) {
+      if (this.left) {
+        return this.left.contains(value);
+      } else {
+        return false;
+      }
+    } else {
+      if (this.right) {
+        return this.right.contains(value);
+      } else {
+        return false;
+      }
+    }
+  }
+
+  remove(value, parent = null) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    if (value < this.value) {
+      if (this.left !== null) {
+        this.left.remove(value, this);
+      }
+    } else if (value > this.value) {
+      if (this.right !== null) {
+        this.right.remove(value, this);
+      }
+    } else {
+      if (this.left !== null && this.right !== null) {
+        this.value = this.right.getMinValue();
+        this.right.remove(this.value, this);
+      } else if (parent === null) {
+        if (this.left !== null) {
+          this.value = this.left.value;
+          this.right = this.left.right;
+          this.left = this.left.left;
+        } else if (this.right !== null) {
+          this.value = this.right.value;
+          this.left = this.right.left;
+          this.right = this.right.right;
+        }
+      } else if (parent.left === this) {
+        parent.left = this.left !== null ? this.left : this.right;
+      } else if (parent.right === this) {
+        parent.right = this.left !== null ? this.left : this.right;
+      }
+    }
+    return this;
+  }
+  getMinValue() {
+    if (this.left === null) {
+      return this.value;
+    } else {
+      return this.left.getMinValue();
+    }
+  }
+}
+
+let bst = new BST(5);
+bst.insert(3);
+console.log(bst.contains(3));
