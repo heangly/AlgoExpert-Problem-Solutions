54d1588785f464fe921454d0a55c3ccc0f5a1f92
Heang Ly <heanglypa@gmail.com>
2020-12-17 09:43:36 -0500

    max subset sum

---
+++ solutions-in-javascript/030.Max-Subset-Sum.js
@@ -0,0 +1,14 @@
+function maxSubsetSumNoAdjacent(array) {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    const current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+  return first;
+}
+
+console.log(maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]));
