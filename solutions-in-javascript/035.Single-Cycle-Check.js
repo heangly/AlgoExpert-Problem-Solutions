b79483ff4d3551ce5a8fbb5b558d500fb9d68c5a
Heang Ly <heanglypa@gmail.com>
2020-12-23 07:50:40 -0500

    single cycle

--- solutions-in-javascript/035.Single-Cycle-Check.js
+++ solutions-in-javascript/035.Single-Cycle-Check.js
@@ -1,5 +1,20 @@
 function hasSingleCycle(array) {
   // Write your code here.
+  let visitedElement = 0;
+  let currIndex = 0;
+  while (visitedElement < array.length) {
+    if (visitedElement > 0 && currIndex === 0) return false;
+    visitedElement++;
+
+    let jump = array[currIndex];
+    let nextIdx = (currIndex + jump) % array.length;
+    if (nextIdx >= 0) {
+      currIndex = nextIdx;
+    } else {
+      currIndex = nextIdx + array.length;
+    }
+  }
+  return currIndex === 0;
 }
 
 console.log(hasSingleCycle([2, 3, 1, -4, -4, 2]));
