5a29d3727dbea0c2535d0a16b85b0094c5bf47fa
Heang Ly <heanglypa@gmail.com>
2020-12-28 08:36:32 -0500

    min-heap

---
+++ solutions-in-javascript/039.Min-Heap-Construction.js
@@ -0,0 +1,35 @@
+// currentNode = i
+// childOne = 2i + 1;
+// childTwo = 2i + 2;
+// parentNode = floor( (i-1) / 2 )
+
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+
+  buildHeap(array) {
+    // Write your code here.
+  }
+
+  siftDown() {
+    // Write your code here.
+  }
+
+  siftUp() {
+    // Write your code here.
+  }
+
+  peek() {
+    // Write your code here.
+  }
+
+  remove() {
+    // Write your code here.
+  }
+
+  insert(value) {
+    // Write your code here.
+    this.heap.push(value);
+  }
+}
