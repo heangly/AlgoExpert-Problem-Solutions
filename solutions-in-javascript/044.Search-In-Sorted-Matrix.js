774757a049a2c128d0104c68f3e519b4ef508a3e
Heang Ly <heanglypa@gmail.com>
2021-01-04 08:20:44 -0500

    search in sorted 2D matrix

---
+++ solutions-in-javascript/044.Search-In-Sorted-Matrix.js
@@ -0,0 +1,26 @@
+function searchInSortedMatrix(matrix, target) {
+  // Write your code here.
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] > target) {
+      col--;
+    } else if (matrix[row][col] < target) {
+      row++;
+    } else {
+      return [row, col];
+    }
+  }
+  return [-1, -1];
+}
+
+const matrix = [
+  [1, 4, 7, 12, 15, 1000],
+  [2, 5, 19, 31, 32, 1001],
+  [3, 8, 24, 33, 35, 1002],
+  [40, 41, 42, 44, 45, 1003],
+  [99, 100, 103, 106, 128, 1004],
+];
+
+console.log(searchInSortedMatrix(matrix, 44));
