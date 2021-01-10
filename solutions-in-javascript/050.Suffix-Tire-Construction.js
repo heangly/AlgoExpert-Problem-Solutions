223331489303badd8322238bef391d093fdf9951
Heang Ly <heanglypa@gmail.com>
2021-01-10 11:24:18 -0500

    suffix

---
+++ solutions-in-javascript/050.Suffix-Tire-Construction.js
@@ -0,0 +1,15 @@
+class SuffixTrie {
+  constructor(string) {
+    this.root = {};
+    this.endSymbol = '*';
+    this.populateSuffixTrieFrom(string);
+  }
+
+  populateSuffixTrieFrom(string) {
+    // Write your code here.
+  }
+
+  contains(string) {
+    // Write your code here.
+  }
+}
