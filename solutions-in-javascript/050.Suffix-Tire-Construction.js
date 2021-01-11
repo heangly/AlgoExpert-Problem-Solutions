81489514bcb4e0e056a6ef107af338fa0f4b9d9e
Heang Ly <heanglypa@gmail.com>
2021-01-11 06:32:38 -0500

    sunffix-tire-construction

--- solutions-in-javascript/050.Suffix-Tire-Construction.js
+++ solutions-in-javascript/050.Suffix-Tire-Construction.js
@@ -7,9 +7,28 @@ class SuffixTrie {
 
   populateSuffixTrieFrom(string) {
     // Write your code here.
+    for (let i = 0; i < string.length; i++) {
+      this.insertSubStringStartingAt(i, string);
+    }
+  }
+
+  insertSubStringStartingAt(i, string) {
+    let node = this.root;
+    for (let j = i; j < string.length; j++) {
+      const letter = string[j];
+      if (!(letter in node)) node[letter] = {};
+      node = node[letter];
+    }
+    node[this.endSymbol] = true;
   }
 
   contains(string) {
     // Write your code here.
+    let node = this.root;
+    for (const letter of string) {
+      if (!(letter in node)) return false;
+      node = node[letter];
+    }
+    return this.endSymbol in node;
   }
 }
