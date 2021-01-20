3047f50d486c713744e40eb2665f1b594ba425b8
Heang Ly <heanglypa@gmail.com>
2021-01-20 07:26:16 -0500

    start zigzag

---
+++ solutions-in-javascript/060.Zigzagg-Traverse.js
@@ -0,0 +1,12 @@
+function zigzagTraverse(array) {
+  // Write your code here.
+}
+
+console.log(
+  zigzagTraverse([
+    [1, 3, 4, 10],
+    [2, 5, 9, 11],
+    [6, 8, 12, 15],
+    [7, 13, 14, 16],
+  ])
+);
