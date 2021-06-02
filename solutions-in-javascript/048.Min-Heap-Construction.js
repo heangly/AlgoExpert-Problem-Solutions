550ffbcbd27f62e5a3e6f0d750d90208f3dcf615
Heang Ly <heanglypa@gmail.com>
2021-06-02 07:55:05 -0400

    set up

---
+++ solutions-in-javascript/048.Min-Heap-Construction.js
@@ -0,0 +1,8 @@
+// ***** Getting childNodes from traditional array format *****
+// Eg. [8, 12, 23, 17, 31, 30, 44, 102, 18]
+// currentNodeIdx = i
+// childOneIdx = 2i + 1
+// childTwoIdx = 2i + 2
+
+// ***** Getting parentNodes from traditional array format *****
+// parentNodeIdx = floor( (i-1) / 2 )
