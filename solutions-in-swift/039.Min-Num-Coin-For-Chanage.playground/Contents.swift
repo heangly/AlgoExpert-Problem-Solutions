640d75c87c2e82c8309479bc49e9df6373bb6717
Heang Ly <heanglypa@gmail.com>
2021-09-21 11:37:03 -0400

    set up

---
+++ solutions-in-swift/039.Min-Num-Coin-For-Chanage.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+func minimumNumberOfCoinsForChange(target: Int, denominations: [Int]) -> Int {
+    // Write your code here.
+    return -1
+}
+
+
+let n = 7
+let denom = [1, 5, 10]
+print(minimumNumberOfCoinsForChange(target: n, denominations: denom))
