0aa07e611b97c424870ec3aac1ca3826d9eeed49
Heang Ly <heanglypa@gmail.com>
2022-01-28 08:28:41 -0500

    topological sort

--- solutions-in-javascript/105-Topological-Sort.js
+++ solutions-in-javascript/105-Topological-Sort.js
@@ -1,18 +1,72 @@
 const topologicalSort = (jobs, deps) => {
-  const obj = {}
-  for (const [first, second] of deps) {
-    if (first in obj) {
-      obj[first].push(second)
-    } else {
-      obj[first] = [second]
+  const jobGraph = createJobGraph(jobs, deps)
+  return getOrderedJobs(jobGraph)
+}
+
+const createJobGraph = (jobs, deps) => {
+  const graph = new JobGraph(jobs)
+  for (const [job, dep] of deps) {
+    graph.addDep(job, dep)
+  }
+  return graph
+}
+
+const getOrderedJobs = (graph) => {
+  const orderedJobs = []
+  const nodesWithNoPrereqs = graph.nodes.filter((node) => !node.numOfPrereqs)
+
+  while (nodesWithNoPrereqs.length) {
+    const node = nodesWithNoPrereqs.pop()
+    orderedJobs.push(node.job)
+    removeDeps(node, nodesWithNoPrereqs)
+  }
+
+  const graphHasEdges = graph.nodes.some((node) => node.numOfPrereqs)
+  return graphHasEdges ? [] : orderedJobs
+}
+
+const removeDeps = (node, nodesWithNoPrereqs) => {
+  while (node.deps.length) {
+    const dep = node.deps.pop()
+    dep.numOfPrereqs--
+    if (!dep.numOfPrereqs) nodesWithNoPrereqs.push(dep)
+  }
+}
+
+class JobGraph {
+  constructor(jobs) {
+    this.nodes = []
+    this.graph = {}
+    console.log(jobs)
+    for (const job of jobs) {
+      this.addNode(job)
     }
   }
 
-  const arr = []
-  Object.values(obj).forEach((val) => arr.push(...val))
-  const childrens = [...new Set(arr)]
+  addDep(job, dep) {
+    const jobNode = this.getNode(job)
+    const depNode = this.getNode(dep)
+    jobNode.deps.push(depNode)
+    depNode.numOfPrereqs++
+  }
 
-  return obj
+  addNode(job) {
+    this.graph[job] = new JobNode(job)
+    this.nodes.push(this.graph[job])
+  }
+
+  getNode(job) {
+    if (!(job in this.graph)) this.addNode(job)
+    return this.graph[job]
+  }
+}
+
+class JobNode {
+  constructor(job) {
+    this.job = job
+    this.deps = []
+    this.numOfPrereqs = 0
+  }
 }
 
 const jobs = [1, 2, 3, 4]
