7c32274f301e76867551077dac333c3aadb37b21
Heang Ly <heanglypa@gmail.com>
2022-03-01 09:25:52 -0500

    ambiguous

--- solutions-in-javascript/118-Ambiguous-Measurements.js
+++ solutions-in-javascript/118-Ambiguous-Measurements.js
@@ -1,4 +1,38 @@
 const ambiguousMeasurements = (measuringCups, low, high) => {
-  // Write your code here.
-  return false
+  const memorization = {}
+  return canMeasureInRange(measuringCups, low, high, memorization)
 }
+
+const canMeasureInRange = (measuringCups, low, high, memorization) => {
+  const memorizeKey = createHashableKey(low, high)
+  if (memorizeKey in memorization) return memorization[memorizeKey]
+  if (low <= 0 && high <= 0) return false
+  let canMeasure = false
+  for (const cup of measuringCups) {
+    const [cupLow, cupHigh] = cup
+    if (low <= cupLow && cupHigh <= high) {
+      canMeasure = true
+      break
+    }
+    const newLow = Math.max(0, low - cupLow)
+    const newHigh = Math.max(0, high - cupHigh)
+    canMeasure = canMeasureInRange(measuringCups, newLow, newHigh, memorization)
+    if (canMeasure) break
+  }
+
+  memorization[memorizeKey] = canMeasure
+  return canMeasure
+}
+
+const createHashableKey = (low, high) => {
+  return low.toString() + ':' + high.toString()
+}
+
+const measuringCups = [
+  [200, 210],
+  [450, 465],
+  [800, 850]
+]
+
+const low = 2100
+const high = 2300
