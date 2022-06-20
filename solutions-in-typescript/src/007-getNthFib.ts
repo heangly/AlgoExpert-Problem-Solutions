98ebb65a6e1f936e9d69aa09f1baa1c994290a40
Heang Ly <heanglypa@gmail.com>
2022-06-20 07:25:05 -0400

    Fib

---
+++ solutions-in-typescript/src/007-getNthFib.ts
@@ -0,0 +1,14 @@
+interface Cache {
+  [key: number]: number
+}
+
+const getNthFib = (n: number, memo: Cache = { 1: 0, 2: 1 }): number => {
+  if (n in memo) return memo[n]
+  memo[n] = getNthFib(n - 1) + getNthFib(n - 2)
+  return memo[n]
+}
+
+console.log(getNthFib(2)) // 1
+console.log(getNthFib(6)) // 5
+
+export {}
