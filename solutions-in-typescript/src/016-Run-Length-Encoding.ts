140c72e6728c29b6d298e151fdc3fb52e83df27a
Heang Ly <heanglypa@gmail.com>
2022-06-29 07:07:18 -0400

    v2

--- solutions-in-typescript/src/016-Run-Length-Encoding.ts
+++ solutions-in-typescript/src/016-Run-Length-Encoding.ts
@@ -1,34 +1,50 @@
 // Time: O(n) | Space: O(n)
 const runLengthEncoding = (string: string): string => {
   const ans: string[] = []
-  let count = 0
-  let runningString = ''
+  let count = 1
 
-  const reset = () => {
-    count = 0
-    runningString = ''
-  }
-
-  for (const str of string) {
-    if (str !== runningString) {
-      if (runningString !== '') {
-        ans.push(`${count}${runningString}`)
-        reset()
-      }
-      runningString = str
-    }
-
-    count++
-
-    if (count === 9) {
-      ans.push(`9${runningString}`)
-      reset()
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1] && count < 9) {
+      count++
+    } else {
+      ans.push(`${count}${string[i]}`)
+      count = 1
     }
   }
-
-  ans.push(`${count}${runningString}`)
 
   return ans.join('')
 }
+
+// const runLengthEncoding = (string: string): string => {
+//   const ans: string[] = []
+//   let count = 0
+//   let runningString = ''
+
+//   const reset = () => {
+//     count = 0
+//     runningString = ''
+//   }
+
+//   for (const str of string) {
+//     if (str !== runningString) {
+//       if (runningString !== '') {
+//         ans.push(`${count}${runningString}`)
+//         reset()
+//       }
+//       runningString = str
+//     }
+
+//     count++
+
+//     if (count === 9) {
+//       ans.push(`9${runningString}`)
+//       reset()
+//     }
+//   }
+
+//   ans.push(`${count}${runningString}`)
+
+//   return ans.join('')
+// }
 
 console.log(runLengthEncoding('AAAAAAAAAAAAABBCCCCDD'))
