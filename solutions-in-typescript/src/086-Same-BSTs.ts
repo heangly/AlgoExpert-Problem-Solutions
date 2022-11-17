9ed95b14fa33bc577950d416dd3e2c7562276b8f
Heang Ly <heanglypa@gmail.com>
2022-11-17 10:08:10 -0500

    start problem 086

---
+++ solutions-in-typescript/src/086-Same-BSTs.ts
@@ -0,0 +1,7 @@
+const sameBsts = (arrayOne: number[], arrayTwo: number[]) => {}
+
+console.log('====================================')
+console.log(
+  sameBsts([10, 15, 8, 12, 94, 81, 5, 2, 11], [10, 8, 5, 15, 2, 12, 11, 94, 81])
+)
+console.log('====================================')
