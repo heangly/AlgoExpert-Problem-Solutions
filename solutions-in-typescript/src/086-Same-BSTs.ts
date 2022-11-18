db540117f911f2a42a9a3edd2623bd0bdf6a5882
Heang Ly <heanglypa@gmail.com>
2022-11-18 07:32:49 -0500

    same bst

--- solutions-in-typescript/src/086-Same-BSTs.ts
+++ solutions-in-typescript/src/086-Same-BSTs.ts
@@ -1,4 +1,36 @@
-const sameBsts = (arrayOne: number[], arrayTwo: number[]) => {}
+// Time: O(n^2) | Space: O(n^2)
+const sameBsts = (arrayOne: number[], arrayTwo: number[]): boolean => {
+  if (arrayOne.length === 0 && arrayTwo.length === 0) return true
+  if (arrayOne[0] !== arrayTwo[0] || arrayOne.length !== arrayTwo.length)
+    return false
+
+  const leftOne = getSmaller(arrayOne)
+  const leftTwo = getSmaller(arrayTwo)
+  const rightOne = getBigger(arrayOne)
+  const rightTwo = getBigger(arrayTwo)
+
+  return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo)
+}
+
+const getSmaller = (array: number[]) => {
+  const smaller: number[] = []
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      smaller.push(array[i])
+    }
+  }
+  return smaller
+}
+
+const getBigger = (array: number[]) => {
+  const bigger: number[] = []
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] >= array[0]) {
+      bigger.push(array[i])
+    }
+  }
+  return bigger
+}
 
 console.log('====================================')
 console.log(
