171eb585efc5918936154f2db2f4e50ff72063ca
Heang Ly <heanglypa@gmail.com>
2023-09-06 08:22:15 -0400

    zig zag

---
+++ solutions-in-typescript/src/025-Zigza-Traverse.ts
@@ -0,0 +1,52 @@
+// Time: O(n * m) | Space: O(n)
+export function zigzagTraverse(array: number[][]) {
+  const height = array.length - 1
+  const width = array[0].length - 1
+  const result: number[] = []
+  let row = 0
+  let col = 0
+  let goingDown = true
+  while (!isOutOfBounds(row, col, height, width)) {
+    result.push(array[row][col])
+    if (goingDown) {
+      if (col === 0 || row === height) {
+        goingDown = false
+        if (row === height) {
+          col++
+        } else {
+          row++
+        }
+      } else {
+        row++
+        col--
+      }
+    } else {
+      if (row === 0 || col === width) {
+        goingDown = true
+        if (col === width) {
+          row++
+        } else {
+          col++
+        }
+      } else {
+        row--
+        col++
+      }
+    }
+  }
+
+  return result
+}
+
+const isOutOfBounds = (row: number, col: number, height: number, width: number) => {
+  return row < 0 || row > height || col < 0 || col > width
+}
+
+console.log(
+  zigzagTraverse([
+    [1, 3, 4, 10],
+    [2, 5, 9, 11],
+    [6, 8, 12, 15],
+    [7, 13, 14, 16]
+  ])
+)
